24434c9e8525ab903ea886628604e6e53b16e510
ELpolloGUAPO <40899576+ELpolloGUAPO@users.noreply.github.com>
2023-05-04 16:56:58 +0300

    Update plot_HOMO_LUMO_energies.py

--- plot_HOMO_LUMO_energies.py
+++ plot_HOMO_LUMO_energies.py
@@ -1,38 +1,63 @@
-'''
+"""
 Script to plot the HOMO and LUMO (plus any amount of neighboring orbitals) orbitals from an output file of FHI-aims.
 Written by Ricardo Ruvalcaba at MONA-group in King Abdullah University of Science and Technology (KAUST).
 Contact: ricardo.ruvalcababriones@kaust.edu.sa
-This is version 2 (19/03/23). No known bugs.
+Version 2 (19/03/23). No known bugs.
+This is version 3 (04/05/23).
+    - Added capability to read file name from command line input.
 
-When executed, the script will ask for all the information.
+When executed, the script will ask for all the necessary information.
 To execute, just run on your bash terminal:
-python3 plot_HOMO_LUMO_energies.py
-'''
+python3 plot_HOMO_LUMO_energies.py FILENAME.out
+
+Or the default:
+python3 plot_HOMO_LUMO_energies.py  # ->  will seek for the file "aims.out",
+                                         otherwise will ask for a correct file name.
+"""
 
 # 0. Import useful packages
 import matplotlib.pyplot as plt
-
+import sys
 
 # 1. Define useful functions and variables
 font_size = 16
-def store_files_in_list(filename):
-    try:
-        with open(filename) as file_in:
-            lines = []
-            for line in file_in:
-                lines.append(line)
-        if filename == 'aims.out':
+
+
+def find_file():
+    # Searches for a file with a given name in the current folder.
+    arguments = sys.argv
+    if len(arguments) > 1:
+        try:
+            filename = sys.argv[1]
+            open(filename)
             print(f'\nFile {filename} found in the current folder.\nOpening now...')
-        if 'Invoking FHI-aims ...' not in lines[1]:
-            print(f'\nERROR: File {filename} is not a FHI-aims output file.')
-            return store_files_in_list(input('Please enter the name of a valid FHI-aims output file:\n'))
-        return lines
-    except FileNotFoundError:
-        if filename == 'aims.out':
-            print(f'\nFile {filename} was not found in the current folder.')
-        else:
+            return filename
+        except FileNotFoundError:
             print(f'\nERROR: File {filename} was not found in the current folder.')
-        return store_files_in_list(input('Please enter the name of a valid FHI-aims output file:\n'))
+            sys.argv[1] = input('Please enter the name of a valid FHI-aims output file: ')
+            return find_file()
+    else:
+        try:
+            open('aims.out')
+            print(f'\nFile aims.out found in the current folder.\nOpening now...')
+            return 'aims.out'
+        except FileNotFoundError:
+            print(f'\nFile aims.out was not found in the current folder.')
+            sys.argv.append(input('Please enter the name of your FHI-aims output file: '))
+            return find_file()
+
+
+def store_files_in_list():
+    filename = find_file()
+    with open(filename) as file_in:
+        lines = []
+        for line in file_in:
+            lines.append(line)
+    if 'Invoking FHI-aims ...' not in lines[1]:
+        print(f'\nERROR: File {filename} is not a FHI-aims output file.')
+        sys.argv[1] = input('Please enter the name of a valid FHI-aims output file:\n')
+        return store_files_in_list()
+    return lines
 
 
 def save_no_of_orbitals(message):
@@ -60,8 +85,7 @@ def ask_type_of_energy():
 
 
 # 2. Determine the number of KS states and the energy tolerance in the system
-file_name = 'aims.out'
-file = store_files_in_list(file_name)
+file = store_files_in_list()
 
 if 'Have a nice day.' not in file[-2]:
     print('\nWARNING: the calculation did not end properly.\nResults may be inaccurate.')
@@ -79,7 +103,7 @@ for index, line in enumerate(file):
         break
 
 no_of_KS_states = int(no_of_KS_states[-1])
-energy_tolerance = float(energy_tolerance[-1]) # in eV
+energy_tolerance = float(energy_tolerance[-1])  # in eV
 energy_tolerance_decimal_string = ("%.10f" % energy_tolerance).rstrip('0').rstrip('.')
 # rounding_criteria = energy_tolerance_decimal_string[::-1].find('.')
 
@@ -101,7 +125,7 @@ if calculation_is_spin_polarized:
         up_orbital[0] = int(up_orbital[0])
         up_orbitals_data.append(up_orbital)
         up_occupation = up_orbital[1]
-        if (up_LUMO_index == None) and (up_occupation < 0.3):
+        if (up_LUMO_index is None) and (up_occupation < 0.3):
             up_LUMO_index = up_orbital[0]
             up_HOMO_index = up_LUMO_index - 1
     for line in file[eigenvalues_index + 5 + no_of_KS_states + 4: eigenvalues_index + 5 + 2*no_of_KS_states + 4]:
@@ -109,46 +133,56 @@ if calculation_is_spin_polarized:
         dn_orbital[0] = int(dn_orbital[0])
         dn_orbitals_data.append(dn_orbital)
         dn_occupation = dn_orbital[1]
-        if (dn_LUMO_index == None) and (dn_occupation < 0.3):
+        if (dn_LUMO_index is None) and (dn_occupation < 0.3):
             dn_LUMO_index = dn_orbital[0]
             dn_HOMO_index = dn_LUMO_index - 1
 
-    # 5. Create a list containing the orbitals to be plotted, print to terminal, and create a dictionary containing the degenerate orbitals according to energy_tolerance criterion
+    # 5. Create a list containing the orbitals to be plotted, print to terminal, and create a dictionary containing the
+    # degenerate orbitals according to energy_tolerance criterion
     up_no_OMOs = save_no_of_orbitals('Enter how many spin-up orbitals below HOMO you want to plot: ')
     if up_HOMO_index - up_no_OMOs <= 0:
         up_max_available_OMO_orbitals = up_HOMO_index - 1
-        print(f'This number exceeds the number of available orbitals.\nThe maximum available ({up_max_available_OMO_orbitals}) will be used instead.\n')
+        print(f'This number exceeds the number of available orbitals.'
+              f'\nThe maximum available ({up_max_available_OMO_orbitals}) will be used instead.\n')
         up_no_OMOs = up_max_available_OMO_orbitals
 
     up_no_UMOs = save_no_of_orbitals('Enter how many spin-up orbitals above LUMO you want to plot: ')
     if up_LUMO_index + up_no_UMOs > no_of_KS_states:
         up_max_available_UMO_orbitals = no_of_KS_states - up_LUMO_index
-        print(f'This number exceeds the number of available orbitals.\nThe maximum available ({up_max_available_UMO_orbitals}) will be used instead.')
+        print(f'This number exceeds the number of available orbitals.'
+              f'\nThe maximum available ({up_max_available_UMO_orbitals}) will be used instead.')
         up_no_UMOs = up_max_available_UMO_orbitals
 
     dn_no_OMOs = save_no_of_orbitals('Enter how many spin-down orbitals below HOMO you want to plot: ')
     if dn_HOMO_index - dn_no_OMOs <= 0:
         dn_max_available_OMO_orbitals = dn_HOMO_index - 1
-        print(f'This number exceeds the number of available orbitals.\nThe maximum available ({dn_max_available_OMO_orbitals}) will be used instead.\n')
+        print(f'This number exceeds the number of available orbitals.'
+              f'\nThe maximum available ({dn_max_available_OMO_orbitals}) will be used instead.\n')
         dn_no_OMOs = dn_max_available_OMO_orbitals
 
     dn_no_UMOs = save_no_of_orbitals('Enter how many spin-down orbitals above LUMO you want to plot: ')
     if dn_LUMO_index + dn_no_UMOs > no_of_KS_states:
         dn_max_available_UMO_orbitals = no_of_KS_states - dn_LUMO_index
-        print(f'This number exceeds the number of available orbitals.\nThe maximum available ({dn_max_available_UMO_orbitals}) will be used instead.')
+        print(f'This number exceeds the number of available orbitals.'
+              f'\nThe maximum available ({dn_max_available_UMO_orbitals}) will be used instead.')
         dn_no_UMOs = dn_max_available_UMO_orbitals
     
     print('\n  Selected eigenvalues:')
     print('  State    Occupation    Eigenvalue [Ha]    Eigenvalue [eV]')
     print('  Spin-up eigenvalues')
-    for line in file[eigenvalues_index + 5 + up_HOMO_index - up_no_OMOs - 1: eigenvalues_index + 5 + up_LUMO_index + up_no_UMOs]:
+    line_lower_limit = eigenvalues_index + 5 + up_HOMO_index - up_no_OMOs - 1
+    line_upper_limit = eigenvalues_index + 5 + up_LUMO_index + up_no_UMOs
+    for line in file[line_lower_limit: line_upper_limit]:
         print(line[:-1])
     print('  Spin-down eigenvalues')
-    for line in file[eigenvalues_index + 5 + no_of_KS_states + 4 + dn_HOMO_index - dn_no_OMOs - 1: eigenvalues_index + 5 + no_of_KS_states + 4 + dn_LUMO_index + dn_no_UMOs]:
+    line_lower_limit = eigenvalues_index + 5 + no_of_KS_states + 4 + dn_HOMO_index - dn_no_OMOs - 1
+    line_upper_limit = eigenvalues_index + 5 + no_of_KS_states + 4 + dn_LUMO_index + dn_no_UMOs
+    for line in file[line_lower_limit: line_upper_limit]:
         print(line[:-1])
 
     print(f'\nEnergy tolerance in your calculation is ({energy_tolerance_decimal_string} eV).')
-    rounding_criteria = save_no_of_orbitals('Enter how many digits after the decimal point you want to consider for the energy (in eV): ')
+    rounding_criteria = save_no_of_orbitals('Enter how many digits after the decimal point you want to'
+                                            ' consider for the energy (in eV): ')
     up_plotted_orbitals = up_orbitals_data[up_HOMO_index - up_no_OMOs: up_LUMO_index + up_no_UMOs + 1]
     up_orbitals_degeneracy = dict()
     orbital_index = -1
@@ -192,8 +226,10 @@ if calculation_is_spin_polarized:
     ax = fig.add_subplot(1, 1, 1)
     max_E_Ha = max(up_orbitals_data[up_LUMO_index + up_no_UMOs][2], dn_orbitals_data[dn_LUMO_index + dn_no_UMOs][2])
     min_E_Ha = min(up_orbitals_data[up_HOMO_index - up_no_OMOs][2], dn_orbitals_data[dn_HOMO_index - dn_no_OMOs][2])
-    ax.plot([-up_max_degeneracy*101, -up_max_degeneracy*100], [max_E_Ha, max_E_Ha], color=LUMO_color, linewidth=2, label='Unoccupied orbitals')
-    ax.plot([-up_max_degeneracy*101, -up_max_degeneracy*100], [min_E_Ha, min_E_Ha], color=HOMO_color, linewidth=2, label='Occupied orbitals')
+    ax.plot([-up_max_degeneracy*101, -up_max_degeneracy*100], [max_E_Ha, max_E_Ha], color=LUMO_color, linewidth=2,
+            label='Unoccupied orbitals')
+    ax.plot([-up_max_degeneracy*101, -up_max_degeneracy*100], [min_E_Ha, min_E_Ha], color=HOMO_color, linewidth=2,
+            label='Occupied orbitals')
     ax.legend(bbox_to_anchor=(1.25, -0.05))
     for rounded_energy in up_orbitals_degeneracy:
         indexes_in_same_energy = up_orbitals_degeneracy[rounded_energy]
@@ -210,7 +246,8 @@ if calculation_is_spin_polarized:
             else:
                 orbital_color = HOMO_color        
             x_position = -up_max_degeneracy/2 - degeneracy/2 + step
-            ax.plot([x_position - bar_width, x_position + bar_width], [plot_energy, plot_energy], color=orbital_color, linewidth=line_width)
+            ax.plot([x_position - bar_width, x_position + bar_width], [plot_energy, plot_energy], color=orbital_color,
+                    linewidth=line_width)
     for rounded_energy in dn_orbitals_degeneracy:
         indexes_in_same_energy = dn_orbitals_degeneracy[rounded_energy]
         degeneracy = len(indexes_in_same_energy)
@@ -226,15 +263,16 @@ if calculation_is_spin_polarized:
             else:
                 orbital_color = HOMO_color        
             x_position = dn_max_degeneracy/2 - degeneracy/2 + step + 1
-            ax.plot([x_position - bar_width, x_position + bar_width], [plot_energy, plot_energy], color=orbital_color, linewidth=line_width)
-    ax2=ax.twinx()
+            ax.plot([x_position - bar_width, x_position + bar_width], [plot_energy, plot_energy], color=orbital_color,
+                    linewidth=line_width)
+    ax2 = ax.twinx()
     ax2.plot([-up_max_degeneracy*101, -up_max_degeneracy*100], [max_E_Ha, max_E_Ha], color='k', linewidth=2)
     ax2.plot([-up_max_degeneracy*101, -up_max_degeneracy*100], [min_E_Ha, min_E_Ha], color='k', linewidth=2)
 
     ax.set(xlim=[-(up_max_degeneracy + 0.5) * 1.1, (dn_max_degeneracy + 0.5) * 1.1])
-    ax.set_xlabel("Spin", fontsize = font_size, labelpad = 0)
-    ax.set_ylabel(r"Energy (eV)", fontsize = font_size, labelpad = 2)
-    ax2.set_ylabel(r"Energy (Ha)", fontsize = font_size, labelpad = 5)
+    ax.set_xlabel("Spin", fontsize=font_size, labelpad=0)
+    ax.set_ylabel(r"Energy (eV)", fontsize=font_size, labelpad=2)
+    ax2.set_ylabel(r"Energy (Ha)", fontsize=font_size, labelpad=5)
     ax.set_xticks([-up_max_degeneracy/2 - 0.5, dn_max_degeneracy/2 + 0.5])
     ax.set_xticklabels([r"$\uparrow$", r"$\downarrow$"], fontsize=font_size)
     ax.tick_params(axis='both', which='major', labelsize=font_size)
@@ -250,26 +288,29 @@ else:
     orbitals_data = [[0, 0, 0, 0]]
     LUMO_index = None
     HOMO_index = None
-    for line in file[eigenvalues_index + 3 : eigenvalues_index + 3 + no_of_KS_states]:
+    for line in file[eigenvalues_index + 3: eigenvalues_index + 3 + no_of_KS_states]:
         orbital = list(map(float, line.split()))
         orbital[0] = int(orbital[0])
         orbitals_data.append(orbital)
         occupation = orbital[1]
-        if (LUMO_index == None) and (occupation < 0.6):
+        if (LUMO_index is None) and (occupation < 0.6):
             LUMO_index = orbital[0]
             HOMO_index = LUMO_index - 1
 
-    # 5. Create a list containing the orbitals to be plotted, print to terminal, and create a dictionary containing the degenerate orbitals according to energy_tolerance criterion
+    # 5. Create a list containing the orbitals to be plotted, print to terminal, and create a dictionary containing the
+    # degenerate orbitals according to energy_tolerance criterion
     no_OMOs = save_no_of_orbitals('Enter how many orbitals below HOMO you want to plot: ')
     if HOMO_index - no_OMOs <= 0:
         max_available_OMO_orbitals = HOMO_index - 1
-        print(f'This number exceeds the number of available orbitals.\nThe maximum available ({max_available_OMO_orbitals}) will be used instead.\n')
+        print(f'This number exceeds the number of available orbitals.'
+              f'\nThe maximum available ({max_available_OMO_orbitals}) will be used instead.\n')
         no_OMOs = max_available_OMO_orbitals
 
     no_UMOs = save_no_of_orbitals('Enter how many orbitals above LUMO you want to plot: ')
     if LUMO_index + no_UMOs > no_of_KS_states:
         max_available_UMO_orbitals = no_of_KS_states - LUMO_index
-        print(f'This number exceeds the number of available orbitals.\nThe maximum available ({max_available_UMO_orbitals}) will be used instead.')
+        print(f'This number exceeds the number of available orbitals.'
+              f'\nThe maximum available ({max_available_UMO_orbitals}) will be used instead.')
         no_UMOs = max_available_UMO_orbitals
     
     print('\n  Selected eigenvalues:')
@@ -279,7 +320,8 @@ else:
         print(line[:-1])
 
     print(f'\nEnergy tolerance in your calculation is ({energy_tolerance_decimal_string} eV).')
-    rounding_criteria = save_no_of_orbitals('Enter how many digits after the decimal point you want to consider for the energy (in eV): ')
+    rounding_criteria = save_no_of_orbitals('Enter how many digits after the decimal point you want to '
+                                            'consider for the energy (in eV): ')
     plotted_orbitals = orbitals_data[HOMO_index - no_OMOs: LUMO_index + no_UMOs + 1]
     orbitals_degeneracy = dict()
     orbital_index = -1
@@ -307,9 +349,11 @@ else:
     ax = fig.add_subplot(1, 1, 1)
     max_E_Ha = plotted_orbitals[-1][2]
     min_E_Ha = plotted_orbitals[-1][2]
-    ax.plot([-max_degeneracy*1.1, -max_degeneracy], [max_E_Ha, max_E_Ha], color=LUMO_color, linewidth=2, label='Unoccupied orbitals')
-    ax.plot([-max_degeneracy*1.1, -max_degeneracy], [min_E_Ha, min_E_Ha], color=HOMO_color, linewidth=2, label='Occupied orbitals')
-    ax.legend(bbox_to_anchor=(1.25,0))
+    ax.plot([-max_degeneracy*1.1, -max_degeneracy], [max_E_Ha, max_E_Ha], color=LUMO_color, linewidth=2,
+            label='Unoccupied orbitals')
+    ax.plot([-max_degeneracy*1.1, -max_degeneracy], [min_E_Ha, min_E_Ha], color=HOMO_color, linewidth=2,
+            label='Occupied orbitals')
+    ax.legend(bbox_to_anchor=(1.25, 0))
     for rounded_energy in orbitals_degeneracy:
         indexes_in_same_energy = orbitals_degeneracy[rounded_energy]
         degeneracy = len(indexes_in_same_energy)
@@ -325,16 +369,17 @@ else:
             else:
                 orbital_color = HOMO_color        
             x_position = -degeneracy/2 + step + 0.5
-            ax.plot([x_position - bar_width, x_position + bar_width], [plot_energy, plot_energy], color=orbital_color, linewidth=line_width)
+            ax.plot([x_position - bar_width, x_position + bar_width], [plot_energy, plot_energy], color=orbital_color,
+                    linewidth=line_width)
     
-    ax2=ax.twinx()
+    ax2 = ax.twinx()
     ax2.plot([-max_degeneracy*1.1, -max_degeneracy], [max_E_Ha, max_E_Ha], color='k', linewidth=2)
     ax2.plot([-max_degeneracy*1.1, -max_degeneracy], [min_E_Ha, min_E_Ha], color='k', linewidth=2)
 
     ax.set(xlim=[-max_degeneracy/2 * 1.1, max_degeneracy/2 * 1.1])
-    ax.set_xlabel("Spin", fontsize = font_size, labelpad = 0)
-    ax.set_ylabel(r"Energy (eV)", fontsize = font_size, labelpad = 2)
-    ax2.set_ylabel(r"Energy (Ha)", fontsize = font_size, labelpad = 5)
+    ax.set_xlabel("Spin", fontsize=font_size, labelpad=0)
+    ax.set_ylabel(r"Energy (eV)", fontsize=font_size, labelpad=2)
+    ax2.set_ylabel(r"Energy (Ha)", fontsize=font_size, labelpad=5)
     ax.set_xticks([0])
     ax.set_xticklabels([r"$\uparrow \downarrow$"], fontsize=font_size)
     ax.tick_params(axis='both', which='major', labelsize=font_size)
